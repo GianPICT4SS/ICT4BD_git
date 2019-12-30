bd6a32edc8b7d20a51409a37b04427401474edb8
gianp <gpdomiziani@gmail.com>
2019-12-30 23:39:16 +0100

    lstm implemented. Relu activation

--- src/method_building.py
+++ src/method_building.py
@@ -29,6 +29,137 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s : %(message)s
 
 logger = logging.getLogger(__name__)
 
+class Prediction():
+
+
+    @classmethod
+    def create_time_steps(cls, length):
+        return list(range(-length, 0))
+
+    @classmethod
+    def show_plot(cls, plot_data, delta, title):
+        labels = ['History', 'True Future', 'Model Prediction']
+        marker = ['.-', 'rx', 'go']
+        time_steps = cls.create_time_steps(plot_data[0].shape[0])
+        if delta:
+            future = delta
+        else:
+            future = 0
+
+        plt.title(title)
+        for i, x in enumerate(plot_data):
+            if i:
+                plt.plot(future, plot_data[i], marker[i], markersize=10,
+                         label=labels[i])
+            else:
+                plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
+        plt.legend()
+        plt.xlim([time_steps[0], (future + 5) * 2])
+        plt.xlabel('Time-Step')
+        return plt
+
+    @classmethod
+    def multivariate_data(cls, dataset, target, start_index, end_index, history_size,
+                          target_size, step, single_step=False):
+        data = []
+        labels = []
+
+        start_index = start_index + history_size
+        if end_index is None:
+            end_index = len(dataset) - target_size
+
+        for i in range(start_index, end_index):
+            indices = range(i - history_size, i, step)
+            data.append(dataset[indices])
+            if single_step:
+                labels.append(target[i + target_size])
+            else:
+                labels.append(target[i:i + target_size])
+
+        return np.array(data), np.array(labels)
+
+    @classmethod
+    def multi_step_plot(cls, history, true_future, prediction):
+        plt.figure(figsize=(12, 6))
+        num_in = cls.create_time_steps(len(history))
+        num_out = len(true_future)
+
+        plt.plot(num_in, np.array(history[:, 1]), label='History')
+        plt.plot(np.arange(num_out), np.array(true_future), 'bo',
+                 label='True Future')
+        if prediction.any():
+            plt.plot(np.arange(num_out), np.array(prediction), 'ro',
+                     label='Predicted Future')
+        plt.legend(loc='upper left')
+        plt.title('Temperature Prediction (LSTM)')
+
+        plt.show()
+
+    @classmethod
+    def plot_train_history(cls, history, title):
+
+        if 'acc' in history.history.keys():
+            acc = history.history['acc']
+            val_acc = history.history['val_acc']
+            loss = history.history['loss']
+            val_loss = history.history['val_loss']
+
+            epochs = range(1, len(loss) + 1)
+
+            fig, (ax1, ax2) = plt.subplots(2, figsize=(8, 8))
+            ax1.plot(epochs, acc, 'bo', label='Training acc')
+            ax1.plot(epochs, val_acc, 'b', label='Validation acc')
+            ax1.set_title('Training and validation accuracy')
+            ax1.legend()
+            ax1.grid(linestyle='--', linewidth=.4, which='both')
+
+            ax2.plot(epochs, loss, 'bo', label='Training loss')
+            ax2.plot(epochs, val_loss, 'b', label='Validation loss')
+            ax2.set_title(title)
+            ax2.legend()
+            ax2.grid(linestyle='--', linewidth=.4, which='both')
+            plt.subplots_adjust(bottom=0.4, right=0.8, top=0.9, hspace=1)
+            plt.savefig(fname=f'../../plots/prediction_model_error_{title}.png', dpi=400)
+            plt.close()
+        else:
+            loss = history.history['loss']
+            val_loss = history.history['val_loss']
+
+            epochs = range(1, len(loss) + 1)
+            fig, ax2 = plt.subplots(1, figsize=(8, 8))
+            ax2.plot(epochs, loss, 'bo', label='Training loss')
+            ax2.plot(epochs, val_loss, 'b', label='Validation loss')
+            ax2.set_title(title)
+            ax2.legend()
+            ax2.grid(linestyle='--', linewidth=.4, which='both')
+            plt.subplots_adjust(bottom=0.4, right=0.8, top=0.9, hspace=1)
+            plt.savefig(fname=f'../../plots/prediction_model_error_{title}.png', dpi=400)
+            plt.close()
+
+    @classmethod
+    def generator(cls, data, lookback, delay, min_index, max_index,
+                  shuffle=False, batch_size=128, step=1):
+        if max_index is None:
+            max_index = len(data) - delay - 1
+        i = min_index + lookback
+        while 1:
+            if shuffle:
+                rows = np.random.randint(min_index + lookback, max_index, size=batch_size)
+            else:
+                if i + batch_size >= max_index:
+                    i = min_index + lookback
+                rows = np.arange(i, min(i + batch_size, max_index))
+                i += len(rows)
+            samples = np.zeros((len(rows), lookback // step, data.shape[-1]))
+            targets = np.zeros((len(rows),))
+            for j, row in enumerate(rows):
+                indices = range(rows[j] - lookback, rows[j], step)
+                samples[j] = data[indices]
+                targets[j] = data[rows[j] + delay][1]
+            yield samples, targets
+
+
+
 
 
 
@@ -57,7 +188,7 @@ def heatmap(X, columns, name):
                 linewidths=.4, cbar_kws={"shrink": .4}, annot=True)
 
     fname = name + '.png'
-    plt.savefig('../plots/' + fname)
+    plt.savefig('../../plots/' + fname)
 
 
 # Neural Network Multi-layer Perceptron regressor
@@ -160,13 +291,13 @@ def parametric_analysis(dinamic_parameter, fixed_parameters, objectives, idf_pat
             df_samples = pd.DataFrame.from_dict(dict_)
             df_samples[parameters[len(fixed_parameters)].selector.object_name] = range_ufw*-1
             problem = EPProblem(parameters, objectives)
-            evaluator = EvaluatorEP(problem, building, epw_file=epw_path, out_dir='../out_dir', err_dir='../err_dir')
+            evaluator = EvaluatorEP(problem, building, epw_file=epw_path, out_dir='../../files/out_dir', err_dir='../../files/err_dir')
             outputs = evaluator.df_apply(df_samples, keep_input=True)
             logger.info(f"Simulation number {j} done.")
             df_samples['DistrictHeating:Facility'] = outputs['DistrictHeating:Facility']
             df_samples['DistrictCooling:Facility'] = outputs['DistrictCooling:Facility']
             df_samples['Electricity:Facility'] = outputs['Electricity:Facility']
-            df_samples.to_csv('../files/outputs/outputs_60p_ch_ufw' + str(j) + '.csv')
+            df_samples.to_csv('../../files/outputs/outputs_60p_ch_ufw' + str(j) + '.csv')
     elif dinamic_parameter['class_name'] == 'Material':
         logger.info(f"Parametric analysis for {dinamic_parameter['object_name']} will start.")
         range_t = np.linspace(0.01, 0.5, n_points)  # metri
@@ -180,39 +311,91 @@ def parametric_analysis(dinamic_parameter, fixed_parameters, objectives, idf_pat
             df_samples = pd.DataFrame.from_dict(dict_)
             df_samples[parameters[len(fixed_parameters)].selector.object_name] = range_t
             problem = EPProblem(parameters, objectives)
-            evaluator = EvaluatorEP(problem, building, epw_file=epw_path, out_dir='../out_dir', err_dir='../err_dir')
+            evaluator = EvaluatorEP(problem, building, epw_file=epw_path, out_dir='../../files/out_dir', err_dir='../../files/err_dir')
             outputs = evaluator.df_apply(df_samples, keep_input=True)
             logger.info(f"Simulation number {j} done.")
             df_samples['DistrictHeating:Facility'] = outputs['DistrictHeating:Facility']
             df_samples['DistrictCooling:Facility'] = outputs['DistrictCooling:Facility']
             df_samples['Electricity:Facility'] = outputs['Electricity:Facility']
             df_samples.to_csv(
-                '../files/outputs/outputs_60p_ch_twr' + str(j) + '.csv')
+                '../../files/outputs/outputs_60p_ch_twr' + str(j) + '.csv')
 
 
 def create_csv(df):
 
-    df1 = pd.DataFrame()
-    for i in df.columns:
-        if '(Hourly)' in i:
-            if 'Zone Operative Temperature' in i or 'Environment:Site Outdoor Air Drybulb Temperature' in i:
-                df1[i] = df[i]
-            elif 'Air Barometric Pressure' in i or 'Site Diffuse Solar Radiation' in i:
-                df1[i] = df[i]
-            elif 'Site Solar Azimuth' in i or 'Site Solar Altitude' in i:
-                df1[i] = df[i]
-            elif 'Zone People Sensible Heating' in i or 'Zone Mean Air Temperature' in i or 'Zone Mean Radiant Temperature' in i:
-                df1[i] = df[i]
-            elif 'Zone Ventilation Sensible Heat' in i or 'Zone Infiltration Sensible Heat' in i:
-                df1[i] = df[i]
-            elif 'Air Change Rate' in i or 'Relative Humidity' in i:
-                df1[i] = df[i]
+    cols = [
+               'Environment:Site Outdoor Air Drybulb Temperature [C](Hourly)',
+               'Environment:Site Outdoor Air Barometric Pressure [Pa](Hourly)',
+               'Environment:Site Diffuse Solar Radiation Rate per Area [W/m2](Hourly)',
+               'Environment:Site Solar Azimuth Angle [deg](Hourly)',
+               'Environment:Site Solar Altitude Angle [deg](Hourly)',
+               'BLOCCO1:ZONA2:Zone Operative Temperature [C](Hourly)',
+               'BLOCCO1:ZONA3:Zone Operative Temperature [C](Hourly)',
+               'BLOCCO1:ZONA1:Zone Operative Temperature [C](Hourly)',
+               'BLOCCO1:ZONA2:Zone Infiltration Sensible Heat Loss Energy [J](Hourly)',
+               'BLOCCO1:ZONA2:Zone Infiltration Sensible Heat Gain Energy [J](Hourly)',
+               'BLOCCO1:ZONA2:Zone Infiltration Air Change Rate [ach](Hourly)',
+               'BLOCCO1:ZONA3:Zone Infiltration Sensible Heat Loss Energy [J](Hourly)',
+               'BLOCCO1:ZONA3:Zone Infiltration Sensible Heat Gain Energy [J](Hourly)',
+               'BLOCCO1:ZONA3:Zone Infiltration Air Change Rate [ach](Hourly)',
+               'BLOCCO1:ZONA1:Zone Infiltration Sensible Heat Loss Energy [J](Hourly)',
+               'BLOCCO1:ZONA1:Zone Infiltration Sensible Heat Gain Energy [J](Hourly)',
+               'BLOCCO1:ZONA1:Zone Infiltration Air Change Rate [ach](Hourly)',
+               'BLOCCO1:ZONA2:Zone Ventilation Sensible Heat Loss Energy [J](Hourly)',
+               'BLOCCO1:ZONA2:Zone Ventilation Sensible Heat Gain Energy [J](Hourly)',
+               'BLOCCO1:ZONA2:Zone Ventilation Air Change Rate [ach](Hourly)',
+               'BLOCCO1:ZONA3:Zone Ventilation Sensible Heat Loss Energy [J](Hourly)',
+               'BLOCCO1:ZONA3:Zone Ventilation Sensible Heat Gain Energy [J](Hourly)',
+               'BLOCCO1:ZONA3:Zone Ventilation Air Change Rate [ach](Hourly)',
+               'BLOCCO1:ZONA1:Zone Ventilation Sensible Heat Loss Energy [J](Hourly)',
+               'BLOCCO1:ZONA1:Zone Ventilation Sensible Heat Gain Energy [J](Hourly)',
+               'BLOCCO1:ZONA1:Zone Ventilation Air Change Rate [ach](Hourly)',
+               'BLOCCO1:ZONA2:Zone Air Relative Humidity [%](Hourly)',
+               'BLOCCO1:ZONA3:Zone Air Relative Humidity [%](Hourly)',
+               'BLOCCO1:ZONA1:Zone Air Relative Humidity [%](Hourly)'
+           ]
 
-    df1['date'] = df['Date/Time'].astype('O')
-    df1['date'] = df1['date'].map(lambda x: x if '24:00' not in x else x.replace('24:00', '00:00'))
-    df1 = df1.set_index('date')
-    df1 = df1.set_index(pd.to_datetime('2018/' + df1.index))
-    return df1
+
+    ren = {
+    'Environment:Site Outdoor Air Drybulb Temperature [C](Hourly)':'Temp_ext[C]',
+    'Environment:Site Outdoor Air Barometric Pressure [Pa](Hourly)':'Pr_ext[Pa]',
+    'Environment:Site Diffuse Solar Radiation Rate per Area [W/m2](Hourly)':'SolarRadiation[W/m2]',
+    'Environment:Site Solar Azimuth Angle [deg](Hourly)':'AzimuthAngle[deg]',
+    'Environment:Site Solar Altitude Angle [deg](Hourly)':'AltitudeAngle[deg]',
+    'BLOCCO1:ZONA2:Zone Operative Temperature [C](Hourly)':'Temp_in2[C]',
+    'BLOCCO1:ZONA3:Zone Operative Temperature [C](Hourly)':'Temp_in3[C]',
+    'BLOCCO1:ZONA1:Zone Operative Temperature [C](Hourly)':'Temp_in1[C]',
+    'BLOCCO1:ZONA2:Zone Infiltration Sensible Heat Loss Energy [J](Hourly)':'InfHeatLoss_2[J]',
+    'BLOCCO1:ZONA2:Zone Infiltration Sensible Heat Gain Energy [J](Hourly)':'InfHeatGain_2[J]',
+    'BLOCCO1:ZONA2:Zone Infiltration Air Change Rate [ach](Hourly)':'InfAirChange_2[ach]',
+    'BLOCCO1:ZONA3:Zone Infiltration Sensible Heat Loss Energy [J](Hourly)':'InfHeatLoss_3[J]',
+    'BLOCCO1:ZONA3:Zone Infiltration Sensible Heat Gain Energy [J](Hourly)':'InfHeatGain_3[J]',
+    'BLOCCO1:ZONA3:Zone Infiltration Air Change Rate [ach](Hourly)':'InfAirChange_3[ach]',
+    'BLOCCO1:ZONA1:Zone Infiltration Sensible Heat Loss Energy [J](Hourly)':'InfHeatLoss_1[J]',
+    'BLOCCO1:ZONA1:Zone Infiltration Sensible Heat Gain Energy [J](Hourly)':'InfHeatGain_1[J]',
+    'BLOCCO1:ZONA1:Zone Infiltration Air Change Rate [ach](Hourly)':'InfAirChange_1[ach]',
+    'BLOCCO1:ZONA2:Zone Ventilation Sensible Heat Loss Energy [J](Hourly)':'VentHeatLoss_2[J]',
+    'BLOCCO1:ZONA2:Zone Ventilation Sensible Heat Gain Energy [J](Hourly)':'VentHeatGain_2[J]',
+    'BLOCCO1:ZONA2:Zone Ventilation Air Change Rate [ach](Hourly)':'VentAirChange_2[ach]',
+    'BLOCCO1:ZONA3:Zone Ventilation Sensible Heat Loss Energy [J](Hourly)':'VentHeatLoss_3[J]',
+    'BLOCCO1:ZONA3:Zone Ventilation Sensible Heat Gain Energy [J](Hourly)':'VentHeatGain_3[J]',
+    'BLOCCO1:ZONA3:Zone Ventilation Air Change Rate [ach](Hourly)':'VentAirChange_3[ach]',
+    'BLOCCO1:ZONA1:Zone Ventilation Sensible Heat Loss Energy [J](Hourly)':'VentHeatLoss_1[J]',
+    'BLOCCO1:ZONA1:Zone Ventilation Sensible Heat Gain Energy [J](Hourly)':'VentHeatGain_1[J]',
+    'BLOCCO1:ZONA1:Zone Ventilation Air Change Rate [ach](Hourly)':'VentAirChange_1[ach]',
+    'BLOCCO1:ZONA2:Zone Air Relative Humidity [%](Hourly)':'Humidity_2[%]',
+    'BLOCCO1:ZONA3:Zone Air Relative Humidity [%](Hourly)':'Humidity_3[%]',
+    'BLOCCO1:ZONA1:Zone Air Relative Humidity [%](Hourly)':'Humidity_1[%]'
+    }
+
+    df_ = pd.DataFrame(df, columns=cols)
+    df_z = df_.rename(columns=ren)
+    df_z['date'] = df['Date/Time'].astype('O')
+    df_z['date'] = df_z['date'].map(lambda x: x if '24:00' not in x else x.replace('24:00', '00:00'))
+    df_z = df_z.set_index('date')
+    df_z = df_z.set_index(pd.to_datetime('2018/' + df_z.index))
+
+    return df_z
 
 
 def energy_signature(iddfile, fname, epw, name=''):
@@ -221,7 +404,7 @@ def energy_signature(iddfile, fname, epw, name=''):
     idf = IDF(fname, epw)
     idf.run(readvars=True)
 
-    fname = 'eplusout' + name + '.csv'
+    fname = 'eplusout_' + name + '.csv'
     os.system(f'cp eplusout.csv ../eplus_simulation/eplus/{fname}')
 
 
@@ -330,3 +513,25 @@ def generator(data, lookback, delay, min_index, max_index,
             samples[j] = data[indices]
             targets[j] = data[rows[j] + delay][1]
         yield samples, targets
+
+def univariate_data(dataset, start_index, end_index, history_size, target_size):
+    data = []
+    labels = []
+
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+        indices = range(i - history_size, i)
+        # Reshape data from (history_size,) to (history_size, 1)
+        data.append(np.reshape(dataset[indices], (history_size, 1)))
+        labels.append(dataset[i+target_size])
+    return np.array(data), np.array(labels)
+
+if __name__ == '__main__':
+
+    df = pd.read_csv('eplus_simulation/eplus/eplusout.csv')
+    df = create_csv(df)
+
+
--- src/predictive_model/temperature_prediction.py
+++ src/predictive_model/temperature_prediction.py
@@ -2,56 +2,212 @@ import pandas as pd
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 import numpy as np
-from src.method_building import create_csv, generator
+from src.method_building import create_csv
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+mpl.rcParams['figure.figsize'] = (8, 6)
 
-from keras.models import Sequential
-from keras import layers
-from keras.optimizers import RMSprop
+import tensorflow as tf
+
+from src.method_building import Prediction
+
+learn = Prediction()
+
+EVALUATION_INTERVAL = 400
+EPOCHS = 22
+BATCH_SIZE = 256
+BUFFER_SIZE = 1000
+
+
 
 
 df = pd.read_csv('../eplus_simulation/eplus/eplusout.csv')
 df = create_csv(df)
-TRAIN = int(df.shape[0]*0.8)
-# parsing the data
-float_data = df.values
-# Normalizing the data
-X_train = df[:TRAIN]
-mean = float_data[:TRAIN].mean(axis=0)
-std = float_data[:TRAIN].std(axis=0)
-float_data_std = (float_data - mean)/std
+TRAIN_SPLIT = int(df.shape[0]*0.8)
+tf.random.set_seed(123)
 
-lookback = 24*30
+
+# =================================================================
+# Recurrent Neural Network(RNN): Long Short Term Memory(LSTM)
+# =================================================================
+
+
+"""
+# ============================================
+# Part 2: Forecast a multivariate TimeSeries
+
+In a multi-step prediction model, given a past history, the model needs to learn to predict a range of future values.
+Thus, unlike a single step model, where only a single future point is predicted, a multi-step model predict a sequence 
+of the future. For the multi-step model, the training data again consists of recordings over the past five days sampled
+every hour. However, here, the model needs to learn to predict the temperature for the next 12 hours. 
+Since an observation is taken every 10 minutes, the output is 72 predictions. For this task, the dataset needs to be 
+prepared accordingly, thus the first step is just to create it again, but with a different target window.
+# ============================================
+"""
+
+features_considered = ['Temp_ext[C]', 'Pr_ext[Pa]', 'SolarRadiation[W/m2]',
+                       'InfHeatLoss_2[J]', 'InfAirChange_2[ach]', 'InfHeatLoss_3[J]',
+                       'InfAirChange_3[ach]', 'InfHeatLoss_1[J]', 'InfAirChange_1[ach]',
+                       'VentHeatLoss_2[J]', 'VentAirChange_2[ach]', 'VentHeatLoss_3[J]',
+                       'VentAirChange_3[ach]', 'VentHeatLoss_1[J]', 'VentAirChange_1[ach]',
+                       'Humidity_1[%]', 'Humidity_2[%]', 'Humidity_3[%]'
+    ]
+
+features = df[features_considered]
+features['Temp_in[C]'] = df[['Temp_in1[C]', 'Temp_in2[C]', 'Temp_in3[C]']].astype(float).mean(1)
+#features['Humidity_in[%]'] = df[['Humidity_1[%]', 'Humidity_2[%]', 'Humidity_3[%]']].astype(float).mean(1)
+features.index = df.index
+
+
+dataset = features.values
+data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+data_std = dataset[:TRAIN_SPLIT].std(axis=0)
+
+dataset = (dataset-data_mean)/data_std
+
+
+past_history = 24*10  # 10 days history
+future_target = 24  # one-day prediction
+STEP = 1
+
+x_train_multi, y_train_multi = learn.multivariate_data(dataset=dataset, target=dataset[:, -1], start_index=0,
+                                                 end_index=TRAIN_SPLIT, history_size=past_history,
+                                                 target_size=future_target, step=STEP)
+x_val_multi, y_val_multi = learn.multivariate_data(dataset=dataset, target=dataset[:, -1],
+                                             start_index=TRAIN_SPLIT, end_index=None, history_size=past_history,
+                                             target_size=future_target, step=STEP)
+
+print('Single window of past history : {}'.format(x_train_multi[0].shape))
+print('\n Target temperature to predict : {}'.format(y_train_multi[0].shape))
+
+train_data_multi = tf.data.Dataset.from_tensor_slices((x_train_multi, y_train_multi))
+train_data_multi = train_data_multi.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_data_multi = tf.data.Dataset.from_tensor_slices((x_val_multi, y_val_multi))
+val_data_multi = val_data_multi.batch(BATCH_SIZE).repeat()
+
+
+#leaky_relu = tf.keras.layers.LeakyReLU(alpha=0.2)
+multi_step_model = tf.keras.models.Sequential()
+multi_step_model.add(tf.keras.layers.LSTM(32,
+                                          dropout=0.2,
+                                          recurrent_dropout=0.5,
+                                          return_sequences=True,
+                                          input_shape=x_train_multi.shape[-2:]))
+multi_step_model.add(tf.keras.layers.LSTM(16,
+                                          dropout=0.2,
+                                          recurrent_dropout=0.5,
+                                          activation='relu'))
+                                          #kernel_initializer='he_normal'))
+
+multi_step_model.add(tf.keras.layers.Dense(future_target))
+
+multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.0008, clipvalue=1.0), loss='mae')
+
+
+multi_step_history = multi_step_model.fit(train_data_multi, epochs=EPOCHS,
+                                          steps_per_epoch=EVALUATION_INTERVAL,
+                                          validation_data=val_data_multi,
+                                          validation_steps=150)
+
+learn.plot_train_history(multi_step_history, 'lr_0.0008')
+
+for x, y in val_data_multi.take(3):
+    learn.multi_step_plot(x[0], y[0], multi_step_model.predict(x)[0])
+
+
+
+
+# whith generator
+"""
+lookback = 24*10
 steps = 1
 delay = 24
-batch_size = 128
+batch_size = 256
 
-train_gen = generator(float_data_std, lookback=lookback,
+train_gen = learn.generator(dataset, lookback=lookback,
                       delay=delay,
                       min_index=0,
-                      max_index=6000,
+                      max_index=TRAIN_SPLIT,
                       shuffle=True,
                       step=steps,
                       batch_size=batch_size)
 
-val_gen = generator(float_data_std, lookback=lookback,
+val_gen = learn.generator(dataset, lookback=lookback,
                       delay=delay,
                       min_index=6001,
-                      max_index=7000,
+                      max_index=7008,
                       step=steps,
                       batch_size=batch_size)
 
-test_gen = generator(float_data_std, lookback=lookback,
+test_gen = learn.generator(dataset, lookback=lookback,
                       delay=delay,
-                      min_index=7001,
+                      min_index=7008,
                       max_index=None,
                       step=steps,
                       batch_size=batch_size)
 
 
-val_steps = (7000 - 6001 - lookback)
-test_steps = (int(df.shape[0]) - 70001 - lookback)
+val_steps = (7008 - 6001 - lookback)
+test_steps = (int(df.shape[0]) - 7008 - lookback)
 
+
+leaky_relu = tf.keras.layers.LeakyReLU(alpha=0.2)
+multi_step_model = tf.keras.models.Sequential()
+multi_step_model.add(tf.keras.layers.GRU(32,
+                                          dropout=0.2,
+                                          recurrent_dropout=0.5,
+                                          return_sequences=True,
+                                          input_shape=(None, dataset.shape[-1])))
+multi_step_model.add(tf.keras.layers.GRU(16,
+                                          dropout=0.2,
+                                          recurrent_dropout=0.5,
+                                          activation=leakly_relu,
+                                         kernel_initializer='he_normal'))
+multi_step_model.add(tf.keras.layers.Dense(24))
+
+multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(clipvalue=1.0, learning_rate=0.0001), loss='mae')
+
+
+multi_step_history = multi_step_model.fit_generator(train_gen, epochs=EPOCHS,
+                                          steps_per_epoch=EVALUATION_INTERVAL,
+                                          validation_data=val_gen,
+                                          validation_steps=50)
+
+learn.plot_train_history(multi_step_history, 'Multi-Step Training and validation loss')
+
+for x, y in val_gen.take(3):
+    learn.multi_step_plot(x[0], y[0], multi_step_model.predict(x)[0])
+
+acc = multi_step_history.history['acc']
+val_acc = multi_step_history.history['val_acc']
+loss = multi_step_history.history['loss']
+val_loss = multi_step_history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+fig, (ax1, ax2) = plt.subplots(2, figsize=(8,8))
+ax1.plot(epochs, acc, 'bo', label='Training acc')
+ax1.plot(epochs, val_acc, 'b', label='Validation acc')
+ax1.set_title('Training and validation accuracy')
+ax1.legend()
+ax1.grid(linestyle='--', linewidth=.4, which='both')
+
+ax2.plot(epochs, loss, 'bo', label='Training loss')
+ax2.plot(epochs, val_loss, 'b', label='Validation loss')
+ax2.set_title('Training and validation loss')
+ax2.legend()
+ax2.grid(linestyle='--', linewidth=.4, which='both')
+plt.subplots_adjust(bottom=0.4, right=0.8, top=0.9, hspace=1)
+plt.savefig(fname='../../plots/prediction_model_error.png', dpi=400)
+plt.close()
+
+"""
+
+
+
+
+""" dropout-regularized Stacked GRU model
 model = Sequential()
 model.add(layers.GRU(32,
                      dropout=0.2,
@@ -67,7 +223,21 @@ history = model.fit_generator(train_gen,
                               validation_steps=val_steps
                               )
 
-""" dropout-regularized Stacked GRU model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 model = Sequential()
 model.add(layers.GRU(32,
                      dropout=0.1,
@@ -87,26 +257,6 @@ history = model.fit_generator(train_gen,
                               validation_steps=val_steps)
 """
 
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
 
-epochs = range(1, len(acc) + 1)
 
-fig, (ax1, ax2) = plt.subplots(2, figsize=(8,8))
-ax1.plot(epochs, acc, 'bo', label='Training acc')
-ax1.plot(epochs, val_acc, 'b', label='Validation acc')
-ax1.set_title('Training and validation accuracy')
-ax1.legend()
-ax1.grid(linestyle='--', linewidth=.4, which='both')
-
-ax2.plot(epochs, loss, 'bo', label='Training loss')
-ax2.plot(epochs, val_loss, 'b', label='Validation loss')
-ax2.set_title('Training and validation loss')
-ax2.legend()
-ax2.grid(linestyle='--', linewidth=.4, which='both')
-plt.subplots_adjust(bottom=0.4, right=0.8, top=0.9, hspace=1)
-plt.savefig(fname='../../plots/prediction_model_error.png', dpi=400)
-plt.close()
 
