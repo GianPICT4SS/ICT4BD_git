59f37c21a6d970873b04e77db5312b65d4b5f644
gianp <gpdomiziani@gmail.com>
2020-01-03 01:36:39 +0100

    temperature_prediction.py improved

--- src/predictive_model/temperature_prediction.py
+++ src/predictive_model/temperature_prediction.py
@@ -3,18 +3,11 @@ import sys
 assert sys.version_info >= (3, 5)
 
 # Scikit-Learn > 0.19 is required
-#import sklearn
+import sklearn
 from sklearn import preprocessing
-#assert sklearn.__version__ >= "0.20"
 
-# TensorFlow >= 2.0 is required
+
 import tensorflow as tf
-
-
-assert tf.__version__ >= "2.0"
-
-if not tf.test.is_gpu_available():
-    print("No GPU was detected: LSTMs and CNNs can be very slow without a GPU.")
 
 # Common imports
 import numpy as np
@@ -40,16 +33,16 @@ learn = Prediction()
 # ===========================================================
 # PARAMETERS
 # ===========================================================
-EVALUATION_INTERVAL = 270
+EVALUATION_INTERVAL = 500
 EPOCHS = 20
-BATCH_SIZE = 156
-BUFFER_SIZE = 1000
+BATCH_SIZE = 126
+BUFFER_SIZE = 5000
 
 # ==============================================================
 # DATASET
 # ===============================================================
-df_ = pd.read_csv('../eplus_simulation/eplus/eplusout.csv')
-df = learn.create_csv(df_)
+df = pd.read_csv('../eplus_simulation/eplus/eplusout.csv')
+df = learn.create_csv(df)
 TRAIN_SPLIT = int(df.shape[0]*0.8)  # 80% data train
 
 # =================================================================
@@ -58,11 +51,8 @@ TRAIN_SPLIT = int(df.shape[0]*0.8)  # 80% data train
 
 
 
-features_considered = ['Temp_ext[C]', 'Pr_ext[Pa]', 'SolarRadiation[W/m2]', 'WindSpeed[m/s]', 'DirectSolarRadiation[W/m2]',
-                       'InfHeatLoss_2[J]', 'InfAirChange_2[ach]', 'InfHeatLoss_3[J]',
-                       'InfAirChange_3[ach]', 'InfHeatLoss_1[J]', 'InfAirChange_1[ach]',
-                       'VentHeatLoss_2[J]', 'VentAirChange_2[ach]', 'VentHeatLoss_3[J]',
-                       'VentAirChange_3[ach]', 'VentHeatLoss_1[J]', 'VentAirChange_1[ach]',
+features_considered = ['Temp_ext[C]', 'Pr_ext[Pa]', 'SolarRadiation[W/m2]', 'WindSpeed[m/s]',
+                       'DirectSolarRadiation[W/m2]', 'AzimuthAngle[deg]', 'AltitudeAngle[deg]',
                        'Humidity_1[%]', 'Humidity_2[%]', 'Humidity_3[%]',
 
     ]
@@ -75,11 +65,11 @@ features.index = df.index
 
 # Standardization data
 dataset = features.values
-#scaler = preprocessing.StandardScaler().fit(dataset)
-#dataset_scaled = scaler.transform(dataset)
-dataset_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
-dataset_std = dataset[:TRAIN_SPLIT].std(axis=0)
-dataset = (dataset-dataset_mean)/dataset_std
+scaler = preprocessing.StandardScaler().fit(dataset)
+dataset = scaler.transform(dataset)
+#dataset_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+#dataset_std = dataset[:TRAIN_SPLIT].std(axis=0)
+#dataset = (dataset-dataset_mean)/dataset_std
 
 past_history = 24*10  # 10 days history
 future_target = 24  # one-day prediction
@@ -91,38 +81,40 @@ x_train_multi, y_train_multi = learn.multivariate_data(dataset=dataset, target=d
                                                  end_index=TRAIN_SPLIT, history_size=past_history,
                                                  target_size=future_target, step=STEP)
 x_val_multi, y_val_multi = learn.multivariate_data(dataset=dataset, target=dataset[:, -1],
-                                             start_index=(TRAIN_SPLIT - int(TRAIN_SPLIT*0.4)), end_index=(TRAIN_SPLIT), history_size=past_history,
+                                             start_index=TRAIN_SPLIT, end_index=None, history_size=past_history,
                                              target_size=future_target, step=STEP)
-x_test_multi, y_test_multi = learn.multivariate_data(dataset=dataset, target=dataset[:, -1],
-                                             start_index=(TRAIN_SPLIT+1), end_index=None, history_size=past_history,
-                                             target_size=future_target, step=STEP)
+
+
+
 
 print('Single window of past history : {}'.format(x_train_multi[0].shape))
 print('Target temperature to predict : {}'.format(y_train_multi[0].shape))
 
 train_data_multi = tf.data.Dataset.from_tensor_slices((x_train_multi, y_train_multi))
-train_data_multi = train_data_multi.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+train_data_multi = train_data_multi.cache().shuffle(BUFFER_SIZE, reshuffle_each_iteration=False).batch(BATCH_SIZE).repeat()
 
 val_data_multi = tf.data.Dataset.from_tensor_slices((x_val_multi, y_val_multi))
 val_data_multi = val_data_multi.batch(BATCH_SIZE).repeat()
 
+# ======================================================
+# Building the LSTM Networks
+# ======================================================
 
 leaky_relu = tf.keras.layers.LeakyReLU(alpha=0.2)
 multi_step_model = tf.keras.models.Sequential()
-multi_step_model.add(tf.keras.layers.LSTM(64,
+multi_step_model.add(tf.keras.layers.LSTM(100,
                                           dropout=0.1,
                                           recurrent_dropout=0.3,
                                           return_sequences=True,
                                           input_shape=x_train_multi.shape[-2:]))
-#multi_step_model.add(tf.keras.layers.LSTM(64,
-#                                          dropout=0.1,
-#                                          recurrent_dropout=0.3,
-#                                          activation='relu',
-#                                          return_sequences=True))
-                                          #kernel_initializer='he_normal'))
 multi_step_model.add(tf.keras.layers.LSTM(32,
+                                          #dropout=0.1,
+                                          #recurrent_dropout=0.3,
+                                          activation='relu',
+                                          return_sequences=True))
+                                          #kernel_initializer='he_normal'))
+multi_step_model.add(tf.keras.layers.LSTM(64,
                                           dropout=0.1,
-                                          recurrent_dropout=0.3,
                                           activation='relu'))
                                           #kernel_initializer='he_normal'))
 
@@ -131,16 +123,18 @@ multi_step_model.add(tf.keras.layers.Dense(future_target))
 print(multi_step_model.summary())
 
 
-#multi_step_model.compile(optimizer=tf.keras.optimizers.Nadam(), loss='mae')
-multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.0001), loss='mae',
+#multi_step_model.compile(optimizer=tf.keras.optimizers.Nadam(), loss='mae', metrics=['accuracy'])
+multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001), loss='mae',
                          metrics=['accuracy'])
 
 
 multi_step_history = multi_step_model.fit(train_data_multi, epochs=EPOCHS,
                                           steps_per_epoch=EVALUATION_INTERVAL,
                                           validation_data=val_data_multi,
-                                          validation_steps=150,
-                                          batch_size=68)
+                                          validation_steps=150)
+
+results = multi_step_model.evaluate(val_data_multi)
+print('Test Acc.: {:.2f}%'.format(results[1]*100))
 
 #learn.plot_train_history(multi_step_history, 'NAdam')
 learn.plot_train_history(multi_step_history, 'RMSprop_lr_0.0001')
@@ -148,13 +142,9 @@ learn.plot_train_history(multi_step_history, 'RMSprop_lr_0.0001')
 for x, y in val_data_multi.take(3):
     learn.multi_step_plot(x[0], y[0], multi_step_model.predict(x)[0])
 
-y_pred_train = multi_step_model.predict(x_train_multi)
-
-y_pred_test = multi_step_model.predict(x_test_multi)
 
 
-for i in range(3):
-    learn.multi_step_plot(x_test_multi[i], y_test_multi[i], y_pred_test[i])
+
 
 
 
