5c679565269be3f29e80e1a5f6cefd346ea1da6f
Riccardo <sappariccardo@gmail.com>
2020-04-04 15:50:01 +0200

    04/04/2020 - 15:48

--- src/predictive_model/TemperaturePredictionClass.py
+++ src/predictive_model/TemperaturePredictionClass.py
@@ -20,16 +20,23 @@ from pathlib import Path
 np.random.seed(69)
 tf.random.set_seed(69)
 
+class NoModelFound(Exception):
+	"""Raised when no model has been loaded"""
+	pass
+
 class TemperatureNN:
 	def __init__(self, path_epout='../eplus_simulation/eplus/eplusout.csv', path_models='models/', path_plots='../../plots/'):
+		self.path_models = path_models
 		self.path_epout = path_epout
 		self.path_plots = path_plots
 		self.learn = Prediction()
-		self.features_considered = ['Temp_ext[C]', 'Pr_ext[Pa]', 'SolarRadiation[W/m2]', 'WindSpeed[m/s]',
-							'DirectSolarRadiation[W/m2]', 'AzimuthAngle[deg]', 'AltitudeAngle[deg]',
-							'Humidity_1[%]', 'Humidity_2[%]', 'Humidity_3[%]',
-							'Heating [J]', 'Cooling [J]', 
-							'Temp_in1[C]', 'Temp_in2[C]', 'Temp_in3[C]']
+		self.features_considered = [
+							'Temp_ext[C]', 'Pr_ext[Pa]', 'SolarRadiation[W/m2]', 
+							'WindSpeed[m/s]','DirectSolarRadiation[W/m2]', 'AzimuthAngle[deg]', 
+							'AltitudeAngle[deg]','Humidity_1[%]', 'Humidity_2[%]', 
+							'Humidity_3[%]', 'Heating [J]', 'Cooling [J]', 
+							'Temp_in1[C]', 'Temp_in2[C]', 'Temp_in3[C]'
+							]
 
 		self.indices = [12,13,14]
 
@@ -67,7 +74,7 @@ class TemperatureNN:
 
 		return xtrain, xtest, xval
 
-	def create_model(self, look_back=24, future=6, n1=50, epochs=30):
+	def create_model(self, look_back=24, future=6, n1=50, epochs=30, train=True, load_model=None):
 		path = Path(self.path_plots)
 		xtrain, xtest, xval = self.create_train_test()
 
@@ -77,32 +84,37 @@ class TemperatureNN:
 		#LSTM needs as input: [samples, timesteps, features]
 		n_outputs = ytrain.shape[2]
 		future = ytrain.shape[1]
-		
-		inp = Input(shape=(xtrain.shape[1], xtrain.shape[2]))
+		if train:
+			inp = Input(shape=(xtrain.shape[1], xtrain.shape[2]))
 
-		gru1 = GRU(n1, return_sequences=True)(inp)
-		gru2 = GRU(n1//2, return_sequences=True, activation='relu', dropout=0.1)(gru1)
-		gru3 = GRU(n1//4, activation='relu', dropout=0.05)(gru2)
+			gru1 = GRU(n1, return_sequences=True)(inp)
+			gru2 = GRU(n1//2, return_sequences=True, activation='relu', dropout=0.1)(gru1)
+			gru3 = GRU(n1//4, activation='relu', dropout=0.05)(gru2)
 
-		#output_layers: 3 layers (3 temperatures) x n_neuros==future (how much in the future to predict)
-		d1 = Dense(future)(gru3)
-		d2 = Dense(future)(gru3)
-		d3 = Dense(future)(gru3)
+			#output_layers: 3 layers (3 temperatures) x n_neuros==future (how much in the future to predict)
+			d1 = Dense(future)(gru3)
+			d2 = Dense(future)(gru3)
+			d3 = Dense(future)(gru3)
 
-		model = Model(inputs=inp, outputs=[d1,d2,d3])
-		
-		print(model.summary())
-		plot_model(model, to_file=f'{path}_Model.png', show_shapes=True, dpi=150)
+			model = Model(inputs=inp, outputs=[d1,d2,d3])
+			
+			print(model.summary())
+			plot_model(model, to_file=f'{path}/Model.png', show_shapes=True, dpi=300)
 
-		model.compile(loss='mae', optimizer='adam')
-		model.fit(xtrain, [ytrain[:,:,i] for i in range(n_outputs)], epochs=epochs, 
-			# validation_data=(xval,[yval[:,:,i] for i in range(n_outputs)]),
-			# validation_steps=200, steps_per_epoch=100,
-			# batch_size=12
-			)
+			model.compile(loss='mae', optimizer='adam')
+			model.fit(xtrain, [ytrain[:,:,i] for i in range(n_outputs)], epochs=epochs, 
+				# validation_data=(xval,[yval[:,:,i] for i in range(n_outputs)]),
+				# validation_steps=200, steps_per_epoch=100,
+				# batch_size=12
+				)
+			name = f'Model_epochs{epochs}_history{look_back}_future{future}'
+			self.save_model(model, name)
 
-		name = f'Model_epochs{epochs}_history{look_back}_future{future}'
-		self.save_model(model, name)
+		else:
+			if load_model == None:
+				raise NoModelFound
+			else:
+				model = load_model
 
 		self.evaluate(model, xtrain, ytrain, 'Train')
 		self.evaluate(model, xtest, ytest)
@@ -111,10 +123,10 @@ class TemperatureNN:
 
 	def save_model(self, model, name):
 		model_json = model.to_json(indent=4)
-		name_json = f'{name}.json'
+		name_json = f'models/{name}.json'
 		with open(name_json, 'w+') as f:
-			f.write(name_json)
-		name_model = f'{name}.h5'
+			f.write(model_json)
+		name_model = f'models/{name}.h5'
 		model.save_weights(name_model)
 		print('Model Saved')
 
@@ -128,26 +140,45 @@ class TemperatureNN:
 			b = results[i,:,:]*self.stds[i] + self.means[i]
 			predictions.append(b)
 			real_data.append(a)
-			test_score = math.sqrt(mean_squared_error(a,b))
+			
+			test_score = math.sqrt(mean_squared_error(b, a))
 			print(f'{name} Score: %.2f RMSE' % (test_score))
 
 			plt.figure(figsize=(15,6))
 			plt.plot(a[:,-1], label='real_data')
 			plt.plot(b[:,-1], label='predictions')
 			plt.legend()
+			plt.grid()
+			plt.title(f'{name}_RealVSPrediction_Temp_in{i+1}[C]')
+			plt.xlabel('TimeSeries size')
+			plt.ylabel('Temperature [C]')
 			plt.savefig(f'{path}/{name}_RealVSPrediction_Temp_in{i+1}[C].png')
+
+	def load_model(self, json_name, model_name, path='models/'):
+		json_file = open(path+json_name, 'r')
+		json_loaded = json_file.read()
+
+		model_loaded = model_from_json(json_loaded)
+		model_loaded.load_weights(path+model_name)
+		print('Model Loaded')
+
+		return model_loaded
 
 if __name__ == '__main__':
 	nn = TemperatureNN()
-	past = 6
-	future = 1
-	name = f"model_history_{past}_ahead_{future}"
+	past = 24
+	future = 6
+	epochs = 20
+
+	name = f"Model_epochs{epochs}_history{past}_future{future}"
 	model_name = f"{name}.h5"
 	json_name = f"{name}.json"
 
-	files = [f for f in os.listdir('.') if os.path.isfile(f)]
-
-	nn.create_model(look_back=past, future=future)
-
+	models = os.listdir('models/')
+	if json_name not in models:	
+		nn.create_model(look_back=past, future=future, epochs=epochs)
+	else:
+		model = nn.load_model(json_name, model_name)
+		nn.create_model(look_back=past, future=future, epochs=epochs, train=0, load_model=model)
 
 		
